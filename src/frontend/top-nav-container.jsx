082e254251260e4c3e1acdb1f070b5cf7553cda0
drewshroyer <drewshroyer@gmail.com>
2020-12-08 14:29:52 -0600

    updated nav bar

--- src/frontend/top-nav-container.jsx
+++ src/frontend/top-nav-container.jsx
@@ -4,26 +4,12 @@ class TopNavContainer extends React.Component {
 render() {
   return (
     <div className="nav-bar-container">
-      <div className="nav-bar-inner-container">
-        <div className="nav-bar-links">
-          <a
-            className="nav-bar-link"
-            id="nav-bar-dash"
-            target="_blank"
-            href={"http://drewshroyer.github.io/"}
-          >
-            Drew Shroyer
-          </a>
-          <a
-            className="nav-bar-link"
-            id="nav-bar-dash"
-            target="_blank"
-            href={"https://www.linkedin.com/in/drew-shroyer-861b32a4/"}
-          >
-            LinkedIn
-          </a>
+        <div className="nav-bar-links">Home
         </div>
-      </div>
+        <div className="nav-bar-title">walk this way
+        </div>
+        <div className="nav-bar-links">Help
+        </div>
     </div>
   );
 }
