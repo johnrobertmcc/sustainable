6b37b17ffda6a6f260ea641a8ffba596b29e78ab
johnrobertmcc <john.robert.mcc@gmail.com>
2020-12-08 17:49:12 -0500

    working with markers

--- src/frontend/map/map.jsx
+++ src/frontend/map/map.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Polyline, DirectionsRenderer, DirectionsService, Marker } from '@react-google-maps/api';
 import key from '../config/key'
  
 const containerStyle = {
@@ -8,9 +8,31 @@ const containerStyle = {
 };
  
 const center = {
-  lat: -3.745,
-  lng: -38.523
+  lat: 48,
+  lng: -120
 };
+
+const path = [
+  {lat: 37.772, lng: -122.214},
+  {lat: 21.291, lng: -157.821},
+  {lat: -18.142, lng: 178.431},
+  {lat: -27.467, lng: 153.027}
+];
+
+const options = {
+  strokeColor: '#FF0000',
+  strokeOpacity: 0.8,
+  strokeWeight: 2,
+  fillColor: '#FF0000',
+  fillOpacity: 0.35,
+  clickable: false,
+  draggable: true,
+  editable: true,
+  visible: true,
+  radius: 30000,
+  zIndex: 9
+};
+
  
 function ShowMap() {
   const [map, setMap] = React.useState(null)
@@ -36,7 +58,10 @@ function ShowMap() {
         onLoad={onLoad}
         onUnmount={onUnmount}
       >
-        <></>
+        <Polyline path={path} options={options}/>
+        <Marker position={{ lat: 48.00, lng: -122.00}} />
+        <Marker position={{ lat: 48.00, lng: -110.00}} />
+     
       </GoogleMap>
     </LoadScript>
   )
