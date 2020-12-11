f001f60482f79e8c5806eed1f03876831f92707d
johnrobertmcc <john.robert.mcc@gmail.com>
2020-12-11 17:47:18 -0500

    added more facts

--- src/frontend/results-modal.jsx
+++ src/frontend/results-modal.jsx
@@ -34,16 +34,38 @@ class ResultsModal extends React.Component {
 
         facts.push( 
             <div className='fun-facts'>
-                    If you made this trip every day, you would have emitted  <span className='num-color'>{carEmit}</span> grams of CO₂ since January 1st by driving! or  <span className='num-color'>{transitEmit}</span> grams of CO₂ since January 1st by taking public transit!
+                    If you made this trip every day, you would have emitted  <span className='num-color'>{carEmit}</span> grams of CO₂ since January 1st by driving, or  <span className='num-color'>{transitEmit}</span> grams of CO₂ since January 1st by taking public transit!
             </div>
             )
 
         facts.push(
             <div className='fun-facts'>
-                The average person emits 27 tons of carbon dioxide a year.
-                If you followed this path every day, you would emit <span className='num-color'>{carSitch}</span> tons just by driving this route,
-                or  <span className='num-color'>{transitSitch}</span> tons just by taking public transit!
-            </div>)
+                The average person emits <span className='num-color'>27 tons</span> of carbon dioxide a year..
+                <br></br>
+                If you followed this path every day, you would emit <span className='num-color'>{carSitch}</span> tons/year just by driving this route,
+                or  <span className='num-color'>{transitSitch}</span> tons/year just by taking public transit!
+            </div>
+        )
+        facts.push(
+            <div className='fun-facts'>
+                Walking and biking are great ways to reduce your carbon footprint!
+            </div>
+        )
+        facts.push(
+            <div className='fun-facts'>
+                But of course, it's not just about CO2! Other contributing factors are:
+                <li>Water vapor: The main greenhouse gas, contributing 36-72 % of the greenhouse effect.</li>
+                <li>Carbon dioxide: The main contribution to the greenhouse effect by human activities.</li>
+                <li>Methane: A very powerful greenhouse gas that is 28 times more potent than carbon dioxide. The levels of methane have increased 170 % since the industrial revolution</li>
+                <li>Ozone: Contributing around 5 % of global warming and has seen a 42 % increase since 1750.</li>
+                <li>Nitrous oxide: An extremely powerful greenhouse gas with a warming potential 265 times higher than carbon.</li>
+
+                <a className="source" href="https://www.theworldcounts.com/challenges/climate-change/global-warming/global-co2-emissions/story" target="_blank">Sources</a>
+                
+            </div>
+        )
+
+
 
     }
 
